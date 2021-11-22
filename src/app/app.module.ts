e992045803e083832816de5767fd509515d391a1
juanmendes <mendesjuan@gmail.com>
2021-11-22 11:15:55 -0500

    Downgrade to clarity 3 and ng 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule } from "@angular/forms";
-import { ClarityModule } from "@clr/angular";
-import { ActivityPromiseResolver } from "@vcd/ui-components";
-import { I18nModule, TranslationService } from "@vcd/i18n";
-import { AppComponent } from "./app.component";
-import translation from "@vcd/ui-components/i18n.json";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { ClarityModule } from '@clr/angular';
+import { AppComponent } from './app.component';
 //__EXAMPLE_MODULE_IMPORT_LINE;
 
 @NgModule({
@@ -15,16 +12,12 @@ import translation from "@vcd/ui-components/i18n.json";
     BrowserAnimationsModule,
     ClarityModule,
     FormsModule,
-    I18nModule.forRoot(),
     //__EXAMPLE_NG_MODULE_IMPORTS_ENTRY
   ],
 
-  providers: [ActivityPromiseResolver,],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {
-  constructor(translationService: TranslationService) {
-    translationService.registerTranslations(translation);
-  }
+  constructor() {}
 }
